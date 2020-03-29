6cea13502080d118f209035b7790234a637b7802
N. Justus <nicola.justus@t-online.de>
2020-03-29 18:01:48 +0200

    add ctrl for seeding database

---
+++ app/controllers/DevController.scala
@@ -0,0 +1,74 @@
+package controllers
+
+import java.time.LocalDate
+
+import javax.inject.{Inject, Singleton}
+import persistence.collections.{BudgetCollection, PaymentCollection}
+import persistence.models.{Account, AuthUser, Budget, Payment}
+import play.api.mvc.{AbstractController, ControllerComponents}
+import reactivemongo.api.bson.BSONObjectID
+import security.AuthenticationService
+import JSONSerializer._
+import com.github.javafaker.Faker
+import io.circe.syntax._
+import play.api.Logging
+import play.api.libs.circe.Circe
+
+import scala.concurrent.Future
+@Singleton
+class DevController @Inject()(cc: ControllerComponents,
+                                 budgetCollection: BudgetCollection,
+                                 paymentCollection: PaymentCollection)
+  extends AbstractController(cc)
+  with Circe
+  with JSONSerializer
+  with Logging {
+
+  implicit val exec = cc.executionContext
+
+  val faker = new Faker()
+  val username = "test"
+
+  def seed = Action.async { req =>
+    val messages = (0 until 3).map { _ =>
+      for {
+        budget <- budget()
+        payments <- Future.sequence(budget.accounts.map(a => payments(a._id)))
+      } yield s"in $budget generated: ${payments.flatten.size} payments"
+    }
+
+    Future.sequence(messages).map(str => Ok(str.asJson))
+  }
+
+  def budget(): Future[Budget] = {
+    val accounts = Seq(
+      Account("commerzbank"),
+      Account("db"),
+    )
+    val b = Budget(
+      faker.internet().domainName(),
+      Some(faker.hitchhikersGuideToTheGalaxy().quote()),
+      accounts,
+      Seq(username),
+      Some(username)
+    )
+
+    budgetCollection.create(b)
+  }
+
+  def payments(accountId: BSONObjectID): Future[Seq[Payment]] = {
+    Future.sequence(
+        (0 until faker.number().numberBetween(0, 100)).map { _ =>
+        val payment = Payment(
+          faker.commerce().productName(),
+          Some(faker.hipster().word()),
+          faker.number().randomDouble(2, -600, 1000),
+          LocalDate.now(),
+          Some(username),
+          accountId
+        )
+        paymentCollection.create(payment)
+      }
+    )
+  }
+}
