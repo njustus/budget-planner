31f21b162341c9ef9be657397ff00729364d1af1
N. Justus <nicola.justus@t-online.de>
2020-03-29 17:40:12 +0200

    add date to payment

--- app/persistence/models/Payment.scala
+++ app/persistence/models/Payment.scala
@@ -1,5 +1,7 @@
 package persistence.models
 
+import java.time.LocalDate
+
 import io.circe.generic.extras.ConfiguredJsonCodec
 import controllers.JSONSerializer._
 import reactivemongo.api.bson._
@@ -8,7 +10,8 @@ import reactivemongo.api.bson._
 case class Payment(name: String,
                    description:Option[String],
                    amount: Double,
+                   date: LocalDate,
+                   owner:Option[String]=None,
                    _accountId: BSONObjectID,
-                  owner:Option[String]=None,
-                  override val _id: BSONObjectID=BSONObjectID.generate())
+                   override val _id: BSONObjectID=BSONObjectID.generate())
   extends BaseEntity(_id, owner)
