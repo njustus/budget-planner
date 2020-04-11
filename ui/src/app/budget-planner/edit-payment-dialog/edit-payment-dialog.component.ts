6f78673af6c71e4da2363a4e5412e2f61e2ffe85
Nicola Justus <nicola.justus@t-online.de>
2020-04-11 18:01:38 +0200

    create payment in dialog

--- ui/src/app/budget-planner/edit-payment-dialog/edit-payment-dialog.component.ts
+++ ui/src/app/budget-planner/edit-payment-dialog/edit-payment-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {Account, Payment} from '../../models'
 import { MAT_DIALOG_DATA } from '@angular/material';
+import { DefaultService } from 'generated-src';
 
 interface PaymentDialogParams {
   accounts: Account[]
@@ -17,12 +18,13 @@ export class EditPaymentDialogComponent implements OnInit {
 
   paymentForm: FormGroup;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public readonly data: PaymentDialogParams) {
+  constructor(@Inject(MAT_DIALOG_DATA) public readonly data: PaymentDialogParams,
+              private readonly apiSvc: DefaultService) {
     const todayStr = new Date().toISOString().substring(0,10)
     this.paymentForm  = new FormGroup({
       name: new FormControl('', [Validators.required]),
       description: new FormControl(''),
-      amount: new FormControl('', [Validators.required]),
+      amount: new FormControl(0.0, [Validators.required]),
       date: new FormControl(todayStr, [Validators.required]),
       _accountId: new FormControl(data.focusedAccount ? data.focusedAccount._id : undefined, [Validators.required]),
     })
@@ -32,6 +34,8 @@ export class EditPaymentDialogComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.paymentForm.value)
+    const input = this.paymentForm.value;
+    const payment = { ...input, amount: parseFloat(input.amount) }
+    this.apiSvc.createPayment(payment).subscribe(p => console.log("payment created: ", p))
   }
 }
