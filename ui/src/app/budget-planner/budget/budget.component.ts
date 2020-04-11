1b8efce22b0782c72ff897018f2dab5c64eb1532
Nicola Justus <nicola.justus@t-online.de>
2020-04-11 16:00:06 +0200

    update focused Account on account tab change

--- ui/src/app/budget-planner/budget/budget.component.ts
+++ ui/src/app/budget-planner/budget/budget.component.ts
@@ -3,6 +3,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { BudgetService } from '../budget.service';
 import { Budget, Account } from 'src/app/models';
 import { flatMap, map } from 'rxjs/operators';
+import { BudgetPlannerService } from '../budget-planner.service';
+import { MatTabChangeEvent } from '@angular/material';
 
 @Component({
   selector: 'app-budget',
@@ -16,6 +18,7 @@ export class BudgetComponent implements OnInit {
   public budget?: Budget
 
   constructor(private readonly route: ActivatedRoute,
+              private readonly budgetPlannerSvc: BudgetPlannerService,
               private readonly budgetSvc: BudgetService) {}
 
   ngOnInit() {
@@ -24,6 +27,8 @@ export class BudgetComponent implements OnInit {
       flatMap(budgetId => this.budgetSvc.findById(budgetId))
     ).subscribe(b => {
       this.budget = b
+      this.budgetPlannerSvc.updateFocusedBudget(b)
+      this.budgetPlannerSvc.updateFocusedAccount(b.accounts[0])
       console.log("budget: ", this.budget)
     })
   }
@@ -31,5 +36,11 @@ export class BudgetComponent implements OnInit {
   get accounts(): Account[] {
     // return (this.budget) ? [BudgetComponent.syntheticAllAccount, ...this.budget.accounts] : [];
     return (this.budget) ? this.budget.accounts : []
+  }
+
+  onTabChange(event: MatTabChangeEvent) {
+    const focusedAccount = this.budget.accounts[event.index]
+    console.log("focused account: ", focusedAccount)
+    this.budgetPlannerSvc.updateFocusedAccount(focusedAccount)
   }
 }
