96acad8b5cb5db6cd26bb9032a59f3538f812ec1
N. Justus <nicola.justus@t-online.de>
2020-04-12 19:10:22 +0200

    add investors to budgets

--- app/controllers/BudgetController.scala
+++ app/controllers/BudgetController.scala
@@ -1,10 +1,13 @@
 package controllers
 
+import io.circe.generic.JsonCodec
+import io.circe.generic.extras.ConfiguredJsonCodec
 import javax.inject.{Inject, Singleton}
 import persistence.collections.BudgetCollection
-import persistence.models.{Account, AuthUser, Budget, Payment}
+import persistence.models.{Account, AuthUser, Budget, Investor, Payment}
 import play.api.mvc.ControllerComponents
 import security.AuthenticationService
+import io.circe.syntax._
 
 @Singleton
 class BudgetController @Inject()(cc: ControllerComponents,
@@ -13,4 +16,13 @@ class BudgetController @Inject()(cc: ControllerComponents,
   extends ResourceController[Budget](cc, budgetCollection) {
 
   override def createRequestEntity(user:AuthUser, budget:Budget): Budget = budget.copy(owner=Some(user.username), investors = Seq(user.username))
+
+  def addInvestor(budgetId: String) = withUser { user =>
+    Action.async(circe.json[Investor]) { implicit request =>
+      budgetCollection.addInvestor(budgetId, request.body.username).map {
+        case Some(b) => Ok(b.asJson)
+        case None => NotFound
+      }
+    }
+  }
 }
