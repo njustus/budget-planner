671f8efcecff1ce0d5eb9d475a57f7007ecdb43b
N. Justus <nicola.justus@t-online.de>
2020-04-25 21:29:38 +0200

    display pagination on payment-list

--- ui/src/app/budget-planner/payment-list/payment-list.component.ts
+++ ui/src/app/budget-planner/payment-list/payment-list.component.ts
@@ -5,8 +5,26 @@ import { DefaultService } from 'generated-src';
 import { MatDialog } from '@angular/material';
 import { EditPaymentDialogComponent } from '../edit-payment-dialog/edit-payment-dialog.component';
 import {PageEvent} from "@angular/material/paginator";
+import {HttpHeaders} from "@angular/common/http";
 
-const rangeRegex = /\d+\s*--\s*\d+\s*\/\s*\d+/gm;
+const RANGE_HEADER = "Content-Range";
+
+interface PageData {
+  pageNumber: number
+  pageSize: number
+  total: number
+}
+
+function parseRangeHeader(pageNumber:number, headers: HttpHeaders): PageData {
+  const value = headers.get(RANGE_HEADER)
+  console.log("header: ", value)
+  const [_, start, end, total] = /(\d+)\s*--\s*(\d+)\s*\/\s*(\d+)/g.exec(value)
+  return {
+    pageNumber: pageNumber,
+    pageSize: 50,
+    total: Number.parseInt(total)
+  }
+}
 
 @Component({
   selector: 'app-payment-list',
@@ -19,12 +37,17 @@ export class PaymentListComponent implements OnInit {
   @Input() public budget: Budget
 
   public payments?: Payment[]
+  public pageData?: PageData
 
   constructor(private readonly apiSvc: DefaultService,
     private readonly dialog: MatDialog) { }
 
   ngOnInit() {
     this.updatePayments()
+  }
+
+  pageChanged(ev: PageEvent) {
+    this.updatePayments(ev.pageIndex+1)
   }
 
   editPayment(payment:Payment) {
@@ -42,8 +65,12 @@ export class PaymentListComponent implements OnInit {
     this.apiSvc.deletePayment(payment._id).subscribe(() => this.updatePayments())
   }
 
-  updatePayments() {
+  updatePayments(page: number = 1) {
     this.payments = undefined
-    this.apiSvc.findAccountPayments(this.account._id).subscribe(xs => this.payments = xs)
+    this.apiSvc.findAccountPayments(this.account._id, page, 'response').subscribe(response => {
+      this.pageData = parseRangeHeader(page, response.headers)
+      console.log("pageData: ", this.pageData)
+      this.payments = response.body
+    })
   }
 }
